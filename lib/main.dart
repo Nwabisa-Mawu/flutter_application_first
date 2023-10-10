defb23d7525be0add962fdea67e63ae6207a4423
Nwabisa Nawu | HAS+ software <techdev3@hireandservice.com>
2023-10-10 10:19:00 +0200

    Completed the home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 // dart library
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+// package that has widgets from material design
 import 'package:flutter/material.dart';
+// Always have to import when you want use pieces of code outside of the file you're working in.
 import 'package:flutter_application_first/screens/intro_screen.dart';
 
+// entry point for the app is the main method, 
 void main() {
-  runApp(const GlobeApp());
+  runApp(const GlobeApp()); //the runApp method inflates a widget and attaches it to the screen - GlobeApp is the first widget we created
 }
 
+// the main app interface that will show on the app
 class GlobeApp extends StatelessWidget {
   const GlobeApp({Key? key}) : super(key: key);
 
+  // use the build method to add the widgets that should be in the app
+  // every widget that extends the stateless widget must override the build method
   @override
   Widget build(BuildContext context) {
+    // this means return a new instance of MaterialApp method which is the main container for apps that use material design.
+    // this is where we will place all the widgets to be seen
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blueGrey),
         // this is code for the home page of the app
--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -9,24 +9,31 @@ class IntroScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       // the appbar gives you the top navigation tab for mobile home page
-      appBar: AppBar(title: const Text('Globo Fitness')),
+      appBar: AppBar(title: Center(child: const Text('Globo Fitness'))),
       // this is where you put the body content of the home page
+      // this is the container for the whole body
       body: Container(
+        // these are the styles for the whole body container with a background image
         decoration: BoxDecoration(
             image: DecorationImage(
           image: AssetImage('assets/beach.jpg'),
           fit: BoxFit.cover,
         )),
+        // this is the start of the textbox inside the body container and it is centered
         child: Center(
             child: Container(
+              // styling for the textbox container
           padding: EdgeInsets.all(24),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(20)),
             color: Colors.white60,
           ),
+          // the text child
           child: Text(
             'Commit to be fit, dare to be great \n with Globo Fitness.',
+            // styling for the text with box shadow effect on text.
             textAlign: TextAlign.center,
+            // in the Textstyle method we put all the styles for the text specifically.
             style: TextStyle(fontSize: 18, shadows: [
               Shadow(
                   offset: Offset(1.0, 1.0), blurRadius: 2.0, color: Colors.grey)
