d68c339ac274b0b1f6bbac1163d719541de6f6af
Nwabisa-Mawu <nwabisa.mawu@gmail.com>
2023-10-17 15:10:00 +0200

    completed add training data

--- lib/data/sp_helper.dart
+++ lib/data/sp_helper.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_function_literals_in_foreach_calls
+
 import 'package:shared_preferences/shared_preferences.dart';
 import './session.dart';
 import 'dart:convert';
@@ -14,5 +16,19 @@ class SPHelper {
   // method that writes a new session into shared preferences
   Future writeSession(Session session) async {
     prefs.setString(session.id.toString(), json.encode(session.toJson()));
+  }
+
+  // method to retrieve all sessions saved into shared preferences - this method is not asynchronous
+  List<Session> getSessions() {
+    List<Session> sessions = [];
+    // used a set because shared preferences returns an unordered list, list is used for an ordered list.
+    Set<String> keys = prefs.getKeys();
+    keys.forEach((String key) {
+      Session session =
+          Session.fromJson(json.decode(prefs.getString(key) ?? ''));
+      // add the session object to the session list
+      sessions.add(session);
+    });
+    return sessions;
   }
 }
--- lib/screens/sessions_screen.dart
+++ lib/screens/sessions_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_function_literals_in_foreach_calls
 
 import 'package:flutter/material.dart';
 import '../data/sp_helper.dart';
@@ -12,6 +12,8 @@ class SessionScreen extends StatefulWidget {
 }
 
 class _SessionScreenState extends State<SessionScreen> {
+  // variable to hold the saved sessions for display in the UI
+  List<Session> sessions = [];
   //textcontroller to get the input from the alert dialog
   final TextEditingController txtDescription = TextEditingController();
   final TextEditingController txtDuration = TextEditingController();
@@ -20,7 +22,9 @@ class _SessionScreenState extends State<SessionScreen> {
 
   @override
   void initState() {
-    helper.init();
+    helper.init().then((value) {
+      updateScreen();
+    });
     super.initState();
   }
 
@@ -30,7 +34,8 @@ class _SessionScreenState extends State<SessionScreen> {
       appBar: AppBar(
         title: Text('Your Training Sessions'),
       ),
-      body: Container(),
+      // to show the sessions, add a ListView widget that has a method to populate it with the information from the sessions list.
+      body: ListView(children: getContent()),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
@@ -40,6 +45,7 @@ class _SessionScreenState extends State<SessionScreen> {
     );
   }
 
+// this shows the dialog when the add button on the page is clicked
   Future<dynamic> showSessionDialog(BuildContext context) async {
     return showDialog(
         context: context,
@@ -86,5 +92,23 @@ class _SessionScreenState extends State<SessionScreen> {
     txtDescription.text = '';
     txtDuration.text = '';
     Navigator.pop(context);
+  }
+
+  // method to display the saved sessions on the UI
+  List<Widget> getContent() {
+    List<Widget> tiles = [];
+    sessions.forEach((session) {
+      tiles.add(ListTile(
+        title: Text(session.description),
+        subtitle: Text('${session.date} - duration: ${session.duration} min'),
+      ));
+    });
+    return tiles;
+  }
+
+  // method to update the UI when a new session is added
+  void updateScreen() {
+    sessions = helper.getSessions();
+    setState(() {});
   }
 }
